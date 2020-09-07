7738f12c456b4626e327b4289c6ca55e892f3941
Sendevia <sendevialol@gmail.com>
2020-09-07 19:57:31 +0800

    更新动画并更改加载方式

---
+++ assets/js/under_construction.js
@@ -0,0 +1,7 @@
+var animation = lottie.loadAnimation({
+    container: document.getElementById('lottie'),
+    path: 'assets/animation/under_construction.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+  })
