845e56674b4afc6bb88716d93fd3ac38c439e6a9
Sendevia <sendevialol@gmail.com>
2020-11-06 13:16:19 +0800

    优化动画

---
+++ assets/js/delimiter.js
@@ -0,0 +1,7 @@
+var animation = lottie.loadAnimation({
+    container: document.getElementById('lottie'),
+    path: 'assets/animation/delimiter.json',
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+  })
