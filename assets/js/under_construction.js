38969715dd2323bf1b9be06146c61c4d78854998
Sendevia <sendevialol@gmail.com>
2020-11-30 22:10:34 +0800

    更新样式

--- assets/js/under_construction.js
+++ assets/js/under_construction.js
@@ -2,6 +2,6 @@ var animation = lottie.loadAnimation({
     container: document.getElementById('lottie'),
     path: 'assets/animation/under_construction.json',
     renderer: 'svg',
-    loop: true,
+    loop: false,
     autoplay: true,
   })
