37cabcccfd3908de22a9ae3ef4f0eb760c3aa26b
Sendevia <sendevialol@gmail.com>
2021-02-21 21:23:45 +0800

    修正文件定位错误

--- assets/js/delimiter.js
+++
@@ -1,7 +0,0 @@
-var animation = lottie.loadAnimation({
-    container: document.getElementById('lottie'),
-    path: 'assets/animation/delimiter.json',
-    renderer: 'svg',
-    loop: false,
-    autoplay: true,
-  })
