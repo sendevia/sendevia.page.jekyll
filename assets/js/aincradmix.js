5e06f8d8e9391163ab6f02ebc53ac0718766ce9f
Sendevia <sendevialol@gmail.com>
2021-03-17 22:04:37 +0800

    更新

--- assets/js/aincradmix.js
+++ assets/js/aincradmix.js
@@ -1,16 +1,6 @@
 //右侧悬浮菜单
-var btn = document.getElementById("float-index-btn");
-var floatIndex = document.getElementById("float-index");
-var btnClose = document.getElementById("float-index-btn-close");
-
-btn.addEventListener("click", function () {
-  floatIndex.style.display = "block";
-});
-btnClose.addEventListener("click", function () {
-  floatIndex.style.display = "none";
-});
-
-window.onscroll = function () {
-  var p = document.documentElement.scrollTop || document.body.scrollTop;
-  var onTop = document.getElementById;
-};
+$(document).ready(function(){
+  $(".float-btn").click(function(){
+    $(".float-index-bg").fadeToggle(500);
+  })
+})
