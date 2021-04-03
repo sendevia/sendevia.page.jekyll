5dae6a5b5a9cf5daa05f34a0114db9f2b1dde3f1
Sendevia <sendevialol@gmail.com>
2021-04-04 00:30:28 +0800

    更新

--- assets/js/aincradmix.js
+++ assets/js/aincradmix.js
@@ -1,14 +1,31 @@
-//右侧悬浮菜单
 $(document).ready(function(){
   $(".float-btn").click(function(){
     $(".float-index-bg").fadeToggle(500);
   })
 })
-//lottie元素_warn
+
 var animation = lottie.loadAnimation({
   container: document.getElementById("warn"),
   path: "assets/animations/warn.json",
   renderer: "svg",
   loop: false,
   autoplay: true,
+});
+
+VanillaTilt.init(document.querySelector(".float-index-content"), {
+  reverse: true,
+  max: 10,
+  scale: 1.05,
+  axis: "y",
+  gyroscope: false,
+});
+
+VanillaTilt.init(document.querySelectorAll(".item-about"), {
+  reverse: true,
+  max: 10,
+  scale: 0.95,
+  axis: "xy",
+  glare: true,
+  "max-glare": 0.3,
+  gyroscope: true,
 });
