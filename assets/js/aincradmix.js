3fb9888569f06a302db98d485e0372a9ee15c0dc
Sendevia <sendevialol@gmail.com>
2021-10-03 14:41:40 +0800

    底层优化

--- assets/js/aincradmix.js
+++ assets/js/aincradmix.js
@@ -2,28 +2,44 @@ $(document).ready(function () {
   $(".toggle-btn").click(function () {
     $(".toggle-item").slideToggle(200);
   });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $(".btn-menu").css({
+        opacity: "0",
+      });
+    } else {
+      $(".btn-menu").css({
+        opacity: "1",
+      });
+    }
+  });
 });
 
-$(window).scroll(function () {
-  if ($(this).scrollTop() > 50) {
-    $(".corner-left,.corner-right").addClass("el-hide");
-  } else {
-    $(".corner-left,.corner-right").removeClass("el-hide");
-  }
-});
-
-lottie.loadAnimation({
+var ani_warn = {
   container: document.getElementById("warn"),
   path: "assets/animations/warn.json",
   renderer: "svg",
   loop: false,
   autoplay: true,
-});
+};
 
-lottie.loadAnimation({
+var ani_errcode = {
   container: document.getElementById("errcode"),
   path: "assets/animations/404.json",
   renderer: "svg",
   loop: false,
   autoplay: true,
-});
+};
+
+var ani_aincrad = {
+  container: document.getElementById("aincrad"),
+  path: "assets/animations/aincrad.json",
+  renderer: "svg",
+  loop: false,
+  autoplay: true,
+};
+
+lottie.loadAnimation(ani_warn);
+lottie.loadAnimation(ani_errcode);
+lottie.loadAnimation(ani_aincrad);
