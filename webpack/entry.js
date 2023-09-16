903a4cdd2b85c455a2e7a769064307c3c7455f06
sendevia <me@sendevia.top>
2023-09-16 14:40:32 +0800

    优化调色板生成器

--- webpack/entry.js
+++ webpack/entry.js
@@ -1,4 +1,4 @@
-import { hexFromArgb, argbFromHex, themeFromImage, themeFromSourceColor } from "@material/material-color-utilities";
+import { hexFromArgb, argbFromHex, sourceColorFromImage, CorePalette } from "@material/material-color-utilities";
 
 /**
  * 需要被取色的图片
@@ -252,11 +252,9 @@ window.onload = function () {
   themeImageProvider.src = contentPhotograph.src;
 
   if (cutsomThemeColor) {
-    generateM3Palette(themeFromSourceColor(argbFromHex(cutsomThemeColor)));
+    generateColorPalette(argbFromHex(cutsomThemeColor));
   } else {
-    themeFromImage(themeImageProvider).then(function (result) {
-      generateM3Palette(result);
-    });
+    generateImagePalette(themeImageProvider);
   }
 };
 
@@ -272,17 +270,135 @@ function removeLoadScreen() {
 }
 
 /**
- * @param theme 一个 theme 对象
+ * 调色板提供器
+ * @param {*} 生成primary调色板
+ * @returns
  */
-function generateM3Palette(theme) {
-  const tones = [0, 4, 6, 10, 12, 17, 20, 22, 24, 30, 40, 50, 60, 80, 87, 90, 92, 94, 95, 96, 98, 99, 100];
+function primaryPalette(source) {
+  const palette = CorePalette.of(source);
+  return {
+    rawPalette: {
+      primary: palette.a1,
+    },
+  };
+}
 
-  for (const [key, palette] of Object.entries(theme.palettes)) {
+/**
+ * 调色板提供器
+ * @param {*} 生成secondary调色板
+ * @returns
+ */
+function secondaryPalette(source) {
+  const palette = CorePalette.of(source);
+  return {
+    rawPalette: {
+      secondary: palette.a2,
+    },
+  };
+}
+
+/**
+ * 调色板提供器
+ * @param {*} 生成tertiary调色板
+ * @returns
+ */
+function tertiaryPalette(source) {
+  const palette = CorePalette.of(source);
+  return {
+    rawPalette: {
+      tertiary: palette.a3,
+    },
+  };
+}
+
+/**
+ * 调色板提供器
+ * @param {*} 生成neutral调色板
+ * @returns
+ */
+function neutralPalette(source) {
+  const palette = CorePalette.of(source);
+  return {
+    rawPalette: {
+      neutral: palette.n1,
+    },
+  };
+}
+
+/**
+ * 调色板提供器
+ * @param {*} 生成neutralVariant调色板
+ * @returns
+ */
+function neutralVariantPalette(source) {
+  const palette = CorePalette.of(source);
+  return {
+    rawPalette: {
+      neutralVariant: palette.n2,
+    },
+  };
+}
+
+/**
+ * 调色板提供器
+ * @param {*} 生成error调色板
+ * @returns
+ */
+function errorPalette(source) {
+  const palette = CorePalette.of(source);
+  return {
+    rawPalette: {
+      error: palette.error,
+    },
+  };
+}
+
+/**
+ * 在DOM中设置调色板
+ * @param {*} toneName 一个色调值列表
+ * @param {*} paletteProvider 调色板提供器
+ */
+function setPaletteProperty(toneName, paletteProvider) {
+  for (const [key, palette] of Object.entries(paletteProvider.rawPalette)) {
     const paletteKey = key.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
-    for (const tone of tones) {
+    for (const tone of toneName) {
       const token = `--md-ref-palette-${paletteKey}${tone}`;
       const color = hexFromArgb(palette.tone(tone));
       themeRoot.style.setProperty(token, color);
     }
   }
+}
+
+/**
+ * 根据图片生成调色板
+ * @param image 输入一张图片
+ * @returns 返回一个完整的调色板
+ */
+async function generateImagePalette(image) {
+  const source = await sourceColorFromImage(image);
+  return generateColorPalette(source);
+}
+
+/**
+ * 根据颜色生成调色板
+ * @param argbColor 输入一个 argb 颜色
+ */
+async function generateColorPalette(argbColor) {
+  const primaryTones = [10, 20, 30, 40, 80, 90, 100];
+  setPaletteProperty(primaryTones, primaryPalette(argbColor));
+
+  const secondaryTones = [10, 20, 30, 40, 80, 90, 100];
+  setPaletteProperty(secondaryTones, secondaryPalette(argbColor));
+
+  const tertiaryTones = [10, 20, 30, 40, 80, 90, 100];
+  setPaletteProperty(tertiaryTones, tertiaryPalette(argbColor));
+
+  const neutralTones = [0, 6, 10, 12, 17, 20, 22, 90, 92, 94, 95, 96, 98];
+  setPaletteProperty(neutralTones, neutralPalette(argbColor));
+
+  const neutralVariantTones = [30, 50, 60, 80, 90];
+  setPaletteProperty(neutralVariantTones, neutralVariantPalette(argbColor));
+
+  const errorTones = [10, 20, 30, 40, 80, 90, 100];
+  setPaletteProperty(errorTones, errorPalette(argbColor));
 }
