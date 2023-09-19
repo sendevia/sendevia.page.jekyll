9b91dcc1f4cf9416ccca903baf188c240ca696e1
Sendevia <me@sendevia.top>
2023-09-19 13:56:18 +0800

    更新到：23.919.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,11 @@
 const path = require("path");
 
-const isProduction = process.env.NODE_ENV == "production";
-
-const config = {
+module.exports = {
+  mode: "development",
   entry: "./webpack/entry.js",
   output: {
     path: path.resolve(__dirname, "assets/js"),
     filename: "bundle.js",
   },
   devtool: "source-map",
-};
-
-module.exports = () => {
-  if (isProduction) {
-    config.mode = "production";
-  } else {
-    config.mode = "development";
-  }
-  return config;
 };
