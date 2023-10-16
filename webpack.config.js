2d4825ed8011bb5218ac5d145c37da693714e151
Sendevia <me@sendevia.top>
2023-10-16 22:22:05 +0800

    修复构建错误

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./webpack/entry.js",
+  entry: "./webpack/app.js",
   output: {
     path: path.resolve(__dirname, "assets/js"),
     filename: "bundle.js",
