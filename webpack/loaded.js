e021dd9650f2d37fce464d2b31e624317c95b6ac
Sendevia <me@sendevia.top>
2023-11-11 16:37:19 +0800

    点击文章h1复制锚点链接

--- webpack/loaded.js
+++ webpack/loaded.js
@@ -1,9 +1,19 @@
 import { argbFromHex } from "@material/material-color-utilities";
 import { generateColorPalette, generateImagePalette } from "./_components/monet";
-import { contentPhotograph, currentPage, customThemeColor, randomRotationBullet, removeLoadScreen, themeImageProvider } from "./app";
+import { contentPhotograph, currentPage, customThemeColor, randomRotationBullet, removeLoadScreen, themeImageProvider, createSnackbar } from "./app";
 
 window.onload = () => {
   randomRotationBullet();
+
+  document.querySelectorAll("#JTM-S-UniversalLayout-ContentFiller h1").forEach((h1) => {
+    h1.addEventListener("click", function () {
+      const anchorLink = this.id ? `#${this.id}` : "";
+
+      if (anchorLink) {
+        navigator.clipboard.writeText(window.location.href.split("#")[0] + anchorLink).then(() => createSnackbar(`已将内容复制到剪贴板：${anchorLink}`));
+      }
+    });
+  });
 
   var testButton = document.getElementById("JTM-P-Components-Snackbar-Test");
   if (testButton) {
