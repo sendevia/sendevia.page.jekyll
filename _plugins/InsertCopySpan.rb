ce19e9be1bfa8c68a9205cda682edd12567e2677
sendevia <me@sendevia.top>
2024-01-24 22:38:27 +0800

    精简代码块，增加一个暂无功能的复制按钮

---
+++ _plugins/InsertCopySpan.rb
@@ -0,0 +1,6 @@
+Jekyll::Hooks.register :documents, :post_render do |doc|
+  if doc.output_ext == ".html"
+    doc.output.gsub!('<div class="highlight">', '<div class="highlight"><span id="copy-button">content_copy</span>')
+    doc.output.gsub!('<figure class="highlight">', '<figure class="highlight"><span id="copy-button">content_copy</span>')
+  end
+end
